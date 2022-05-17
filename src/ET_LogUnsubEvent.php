f88dedb5a2b70235a53ea13f80f331050a812d9e
Adam Stitzer <astitzer@gmail.com>
2022-05-17 12:53:25 -0400

    add LogUnsubEvent

--- src/ET_LogUnsubEvent.php
+++ src/ET_LogUnsubEvent.php
@@ -6,11 +6,25 @@
 class ET_LogUnsubEvent extends ET_CUDSupport
 {
   /** 
-	* Initializes a new instance of the class and sets the obj property of parent.
-	*/	
+	 * Initializes a new instance of the class and sets the obj property of parent.
+	 */	
 	function __construct()
 	{
 		$this->obj = "LogUnsubEvent";
+	}
+
+	/**
+	 * Send this instance.
+	 * @return ET_Post     Object of type ET_Post which contains http status code, response, etc from the POST SOAP service
+	 */	
+	public function Send()
+	{
+		$er = [
+			'Name' => 'LogUnsubEvent',
+			'Parameters' => $this->props,
+		];
+		$response = new ET_Execute($this->authStub, "ExecuteRequest", $er);
+		return $response;
 	}
 }
 ?>
